0925bde1b11a410fbb9cf788d0cf4340da0d5fe5
Sivaraj Nagaraj <sivarajng@gmail.com>
2017-11-27 17:19:44 +0530

    minor

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -7,7 +7,7 @@ export class AuthGuard implements CanActivate {
     constructor(private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        localStorage.setItem('currentUser','currentUser')
+      
         if (localStorage.getItem('currentUser')) {
             // logged in so return true
             return true;
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private user:object = {user:'',password:''};
+  private error:String = '';
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  login(){
+
+    if(){
+      localStorage.setItem('currentUser','currentUser');
+      this.router.navigate(['/profile']);
+    }
+    else{
+
+    }
+
+  }
 }
